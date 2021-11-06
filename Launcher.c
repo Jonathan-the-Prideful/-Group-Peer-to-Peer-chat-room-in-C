e3f7543d22fa8c443aadb1cb3b7e92db3b71bba0
Jonathan-the-Prideful <93452788+Jonathan-the-Prideful@users.noreply.github.com>
2021-11-06 19:08:48 -0400

    Update Launcher.c

--- Launcher.c
+++ Launcher.c
@@ -1,12 +1,21 @@
-#include <gtk/gtk.h>
+include <gtk/gtk.h>
 
 int main (int argc, char **argv){
 
+    //So this is used to call visual and color maping and initalizes the gtx library.
     gtk_init (&argc, &argv);
 
-    //gtk code comes here
+    //window library extends to window
+    GtkWidget *window;
 
-    gtk_main();
+    //Shows 
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show_all(window);
+
+    //close window corectly ending program.
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_main ();// loops the function
 
     return 0;
 }
