d37748c0dc93dd38e6007d0f047267266d5e0cca
Jonathan-the-Prideful <93452788+Jonathan-the-Prideful@users.noreply.github.com>
2021-11-08 14:48:31 -0500

    Window placed, sized correctly, and is locked at set size.

--- Launcher.c
+++ Launcher.c
@@ -1,4 +1,6 @@
-include <gtk/gtk.h>
+#include <gtk/gtk.h>
+#define WINDOW_WIDTH 700
+#define WINDOW_HEIGHT 500
 
 int main (int argc, char **argv){
 
@@ -8,8 +10,14 @@ int main (int argc, char **argv){
     //window library extends to window
     GtkWidget *window;
 
-    //Shows 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    //Displays the window
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);// Main window
+    
+    gtk_window_set_title(GTK_WINDOW(window), "The Awsome Chat Room!");// set title of application
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);// set size in pixles
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);//center window
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);// prevent user from resizing window
+
     gtk_widget_show_all(window);
 
     //close window corectly ending program.
