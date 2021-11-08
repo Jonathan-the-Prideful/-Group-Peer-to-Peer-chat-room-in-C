52b730d8d1f36674916d74d1158c0cca31804277
Jonathan-the-Prideful <93452788+Jonathan-the-Prideful@users.noreply.github.com>
2021-11-08 15:47:14 -0500

    Update to inizlize the program

--- Launcher.c
+++ Launcher.c
@@ -2,12 +2,35 @@
 #define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 500
 
+void inizilize (GtkWidget *window){
+    GtkWidget *buttonServer;
+    GtkWidget *buttonClient;
+    GtkWidget *box;
+    GtkWidget *instructions;
+
+    instructions = gtk_label_new("What would you like to do?");
+
+    //setup box to contain items
+    box = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(window), box);//add box to window
+
+    //Add buttons
+    buttonServer = gtk_button_new_with_label("Host Chat");
+    gtk_widget_set_size_request(GTK_WIDGET(buttonServer), 200, 100);
+    buttonClient = gtk_button_new_with_label("Join Chat");
+    gtk_widget_set_size_request(GTK_WIDGET(buttonClient), 200, 100);
+    gtk_fixed_put(GTK_FIXED(box), buttonServer, 100, 250);
+    gtk_fixed_put(GTK_FIXED(box), buttonClient, 400, 250);
+    gtk_fixed_put(GTK_FIXED(box), instructions, (WINDOW_WIDTH / 2) - 80, 50);
+
+}
+
 int main (int argc, char **argv){
 
     //So this is used to call visual and color maping and initalizes the gtx library.
     gtk_init (&argc, &argv);
 
-    //window library extends to window
+    //widget library extends to window
     GtkWidget *window;
 
     //Displays the window
@@ -17,6 +40,8 @@ int main (int argc, char **argv){
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);// set size in pixles
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);//center window
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);// prevent user from resizing window
+
+    inizilize(window);// pass the window to inizlize
 
     gtk_widget_show_all(window);
 
