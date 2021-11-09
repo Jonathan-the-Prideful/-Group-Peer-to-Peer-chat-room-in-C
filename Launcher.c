62e29edf0dd9e469156896637e9753f2fa3b7da7
Jonathan-the-Prideful <93452788+Jonathan-the-Prideful@users.noreply.github.com>
2021-11-09 00:25:50 -0500

    Update add client

--- Launcher.c
+++ Launcher.c
@@ -3,6 +3,23 @@
 #define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 500
 
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#define MAXPORT 80
+#define PORT 8080
+#define SA struct sockaddr
+int sockfdGlobe;
+char buffRead[MAXPORT];
+int is_Alive = 1;
+pthread_mutex_t stdout_lock;
+int readCheck = 0;
+
 GtkWidget *buttonServer,*buttonClient; //buttons
 GtkWidget *box; //fields
 GtkWidget *instructions; //labels
@@ -14,6 +31,7 @@ void entered(GtkEntry *widget, gpointer data){
     g_print("you entered: %s\n", gtk_entry_get_text(GTK_ENTRY(data)));
     gchar* IP  = gtk_entry_get_text(GTK_ENTRY(data));
     g_print("IP = %s \n", IP);
+    chatClient();
 }
 
 void runClient(){
@@ -67,6 +85,110 @@ void inizilize (GtkWidget *window){
     g_signal_connect(buttonServer, "clicked", G_CALLBACK(button_clicked), "server");
     g_signal_connect(buttonClient, "clicked", G_CALLBACK(button_clicked), "client");
     
+}
+
+/*
+ * client.c
+ *
+ *  Created on: Nov 11, 2020
+ *      Author: sarwars1
+ */
+void *read2(void *_args){
+	char buff[MAXPORT];
+	for(;;){
+		readCheck = 0;
+		bzero(buff, MAXPORT);
+		read(sockfdGlobe, buff, sizeof(buff));
+		readCheck = 1;
+		fprintf(stdout, "\nFrom other user: %s", buff);
+			// if msg contains "Exit" then server exit and chat ended.	
+		//pthread_mutex_unlock(&stdout_lock);
+		readCheck = 1;
+		if (is_Alive == 0) {
+			printf("Read thread exit\n");
+			break;
+		}
+	}
+	pthread_exit(NULL);
+}
+void *write2(void *_args){
+	
+	int n;
+	is_Alive = 1;
+	for(;;){
+		bzero(buffRead, sizeof(buffRead)); 
+		fprintf(stdout, "Enter a string: ");
+		n = 0;
+		bzero(buffRead, MAXPORT);
+		//pthread_mutex_lock(&stdout_lock);
+		while ((buffRead[n++] = getchar()) != '\n'){
+			if(readCheck == 1){
+				fprintf(stdout, "Enter a string: %s", buffRead);
+			}
+		};
+		//sleep(2);
+		//pthread_mutex_unlock(&stdout_lock);
+		write(sockfdGlobe, buffRead, sizeof(buffRead));
+		if ((strncmp(buffRead, "exit", 4)) == 0) {
+			printf("Client Exit...\n");
+			is_Alive = 0;
+		}
+		// if msg contains "Exit" then server exit and chat ended.
+		if (is_Alive == 0) {
+			printf("client exit \n");
+			break;
+		}
+	}
+	
+	pthread_exit(NULL);
+}
+void func(int sockfd)
+{
+	if (pthread_mutex_init(&stdout_lock, NULL) != 0) {
+		printf("\n mutex init has failed\n");
+	}
+	pthread_t thread_id1;
+	pthread_t thread_id2;
+	sockfdGlobe = sockfd;
+	pthread_create(&thread_id1, NULL, read2, NULL);
+	pthread_create(&thread_id2, NULL, write2, NULL);
+	pthread_join(thread_id1, NULL);
+	pthread_join(thread_id2, NULL);
+}
+
+int chatClient()
+{
+	int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+	// socket create and varification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(PORT);
+
+	// connect the client socket to server socket
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+		printf("connection with the server failed...\n");
+		exit(0);
+	}
+	else
+		printf("connected to the server..\n");
+
+	// function for chat
+	func(sockfd);
+
+	// close the socket
+	close(sockfd);
 }
 
 int main (int argc, char **argv){
